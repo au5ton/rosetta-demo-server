7c2caeaea61fa46e6227c1502d46b62a294c3736
Austin Jackson <themacphage@gmail.com>
2021-06-30 12:50:48 -0500

    this is a bad idea

--- lib/firebaseFront.ts
+++ lib/firebaseFront.ts
@@ -1,5 +1,10 @@
 import * as firebase from 'firebase'
 
-export const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG ?? '');
+/**
+ * i hate that i'm doing this, but Vercel doesn't have a good way to include secrets 
+ * other than environment variables and the Google Translate service account key 
+ * takes up the entire 4KB limit...
+ */
+export const firebaseConfig = {"apiKey":"AIzaSyDz7fwvnHknXfZB-vEiIZRkyqv4-m1ZsZo","projectId":"rosetta-demo-server"};
 
 export const Timestamp = firebase.default.firestore.Timestamp;
